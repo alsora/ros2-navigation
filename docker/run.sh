5ab4d16c9dd65f145d79994038282e9d5aa6ad92
Soragna, Alberto <alberto.soragna@gmail.com>
2020-02-29 11:48:05 +0000

    use  instead of /home/asoragna/repos/ros2/ros2-navigation in run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,17 +3,21 @@
 # @author Alberto Soragna (alberto dot soragna at gmail dot com)
 # @2020
 
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 IMG_NAME="ros2_eloquent"
 
 NETWORK_SETTINGS="--net=host --privileged"
 
 XSOCK=/tmp/.X11-unix
-DISPLAY_SETTINGS="-e DISPLAY="$DISPLAY" -v "$XSOCK":"$XSOCK" -v "$XAUTHORITY":/root/.Xauthority"
+DISPLAY_SETTINGS="-e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTHORITY:/root/.Xauthority"
 
 if [[ $1 == "--dev" ]]; then
-    DEV_SETTINGS="-v "$PWD"/..:/root/my_repo"
+    # If developer mode, mount the whole repository
+    DEV_SETTINGS="-v $THIS_DIR/..:/root/my_repo"
 else
-    DEV_SETTINGS=""
+    # Else mount only the simulation script in read only mode
+    DEV_SETTINGS="-v $THIS_DIR/../simulation.sh:/root/simulation.sh:ro"
 fi
 
 ENTRY_CMD="bash"
