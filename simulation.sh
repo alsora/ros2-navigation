b6eac47ab89f8a5951d99d4e7cee8f6e1a8662eb
Soragna, Alberto <alberto.soragna@gmail.com>
2020-02-29 10:50:26 +0000

    cleanup simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -2,7 +2,9 @@
 #
 # @author Alberto Soragna (alberto dot soragna at gmail dot com)
 # @2020
- 
+
+THIS_SCRIPT=`basename "$0"`
+
 main() {
 
     parse_arguments "$@"
@@ -20,24 +22,22 @@ main() {
     # Make sure gazebo is not already running
     kill_gazebo
 
-    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$HOME/turtlebot_ws/src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models
     export TURTLEBOT3_MODEL=${ROBOT}
 
     # Start gazebo
-    xterm -hold -e "gazebo --verbose -s libgazebo_ros_init.so /opt/ros/eloquent/share/nav2_bringup/worlds/$TURTLEBOT3_MODEL.model"&
+    execute "gazebo --verbose -s libgazebo_ros_init.so /opt/ros/eloquent/share/nav2_bringup/worlds/$TURTLEBOT3_MODEL.model"
     
     # Start TF publisher
-    xterm -hold -e "sleep 15; ros2 launch turtlebot3_bringup turtlebot3_state_publisher.launch.py use_sim_time:=True"&
+    execute "sleep 15; ros2 launch turtlebot3_bringup turtlebot3_state_publisher.launch.py use_sim_time:=True"
     
     # Start Rviz
-    xterm -hold -e "sleep 20; rviz2 -d /opt/ros/eloquent/share/nav2_bringup/rviz/nav2_default_view.rviz"&
+    execute "sleep 20; rviz2 -d /opt/ros/eloquent/share/nav2_bringup/rviz/nav2_default_view.rviz"
     
     # Start ROS 2 Navigation Stack
-    xterm -hold -e "sleep 25; ros2 launch nav2_bringup nav2_bringup_launch.py use_sim_time:=True autostart:=True map:=/opt/ros/eloquent/share/nav2_bringup/maps/turtlebot3_world.yaml"&
+    execute "sleep 25; ros2 launch nav2_bringup nav2_bringup_launch.py use_sim_time:=True autostart:=True map:=/opt/ros/eloquent/share/nav2_bringup/maps/turtlebot3_world.yaml"
 }
 
 parse_arguments() {
-    # Parse command line arguments
     POSITIONAL=()
     while [[ $# -gt 0 ]]
     do
@@ -66,7 +66,7 @@ kill_simulation() {
     pkill -9 xterm
     kill_gazebo
     
-    echo "Killed everything!"
+    echo "[$THIS_SCRIPT] Killed everything!"
     exit 0
 }
 
@@ -74,6 +74,13 @@ kill_gazebo() {
     pkill -9 gzclient
     pkill -9 gzserver
     pkill -9 gazebo
+}
+
+execute() {
+    if [ "$#" -ne 1 ]; then
+        echo "[$THIS_SCRIPT] execute requires exactly 1 argument"
+    fi
+    xterm -hold -e "$1" &
 }
 
 
